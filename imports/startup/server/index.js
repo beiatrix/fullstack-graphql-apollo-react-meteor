472531bbaffae00bc0370cf89bdc4b7482f59cfb
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-05-27 19:01:52 -0700

    First schema & query

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,33 @@
 import { createApolloServer } from 'meteor/apollo'
 import { makeExecutableSchema } from 'graphql-tools'
 
-createApolloServer({})
+// not javascript - graphql syntax
+// graphql schema
+// query is essentially a function, need to define function in schema
+const typeDefs = `
+type Query {
+    hi: String
+}
+`
+
+// this is javascript
+// actual method - server side code
+const resolvers = {
+    Query: {
+        hi() {
+            return "Hello Level Up"
+        }
+    }
+}
+
+// in graphiql, just type:
+// {
+//     hi
+// }
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+createApolloServer({schema})
